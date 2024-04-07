a36ddc6dc097b5949dbfb35cc189dea2298d172e
masaya2983 <127204441+masaya2983@users.noreply.github.com>
2024-04-07 14:46:54 +0900

    勉強

---
+++ sample2/Sample2_5.java
@@ -0,0 +1,19 @@
+package Sample2;
+class Sample2_5{
+	public static void main_(String[] args) {
+		char[] C;
+		C = new char [3];
+		int[] i = new int[3];
+		String str[] = {"Welcome","to","Java"};
+		C[0] = 'A'; C[1] ='B'; C[2] = 'C';
+		i[0] = 100;i[1] = 200;  i[2] = 300;
+		System.out.println("str[]のサイズ:" +str.length);
+		System.out.println("c[0]    :"+C[0]);
+		System.out.println("i[1]     :"+ i[1]);
+		System.out.println(C);
+		System.out.println(i);
+		System.out.println(str);
+		
+		
+	}
+}
