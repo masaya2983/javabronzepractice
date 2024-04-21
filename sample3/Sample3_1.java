8a4ac7cd1d8b9651ddc882e299827ba147b3991b
masaya2983 <127204441+masaya2983@users.noreply.github.com>
2024-04-21 23:06:07 +0900

    勉強

---
+++ sample3/Sample3_1.java
@@ -0,0 +1,10 @@
+package sample3;
+ class Sample3_1{
+	 public static void main(String[] args) {
+		 System.out.println(10 + 3);
+		 System.out.println(10 - 3);
+		 System.out.println(10 * 3);
+		 System.out.println(10 / 3);
+		 System.out.println(10 % 3);
+	 }
+ }
