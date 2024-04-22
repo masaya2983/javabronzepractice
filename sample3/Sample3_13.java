b174314d23772f69cbb7dddfc6fda6244f7444c4
masaya2983 <127204441+masaya2983@users.noreply.github.com>
2024-04-23 01:46:27 +0900

    勉強

---
+++ sample3/Sample3_13.java
@@ -0,0 +1,23 @@
+package sample3;
+class Sample3_13{
+	public static void main(String []args) {
+		int num = 3;
+		
+		switch (num) {
+		case 1:
+		case 2:
+			System.out.println("1または2"); //num値が1か2の場合の処理
+			break;
+		case 3:
+		case 4:
+			System.out.println("3または4");
+			break;
+			default:
+			System.out.println("default");
+			
+			
+			
+		
+		}
+	}
+}
