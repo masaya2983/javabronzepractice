e0c57cfae4b0908cc095fd4c0b67a19639c8e06f
masaya2983 <127204441+masaya2983@users.noreply.github.com>
2024-04-29 23:39:44 +0900

    勉強

---
+++ sample2/problem2/problem2-9.java
@@ -0,0 +1,9 @@
+public class Test{
+	public static void main (String []args)
+	char arry1[] = {'a','b','c','d','e'};
+	char arry2[] = new char[10];
+	arry2[0] = 'f';
+	array2[1] = 'g';
+	System.out.println(array1.length+""+arry2.length);
+	
+}
